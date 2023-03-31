f938fc0079c912470491f2af21c3b3a72d8dc045
Nikolay_Cholakov <nikolaicholakov25@gmail.com>
2023-03-31 13:07:24 +0300

    Added Google meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,10 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="lirRLS53NRFDILy-KzmKtZHZmfRiilm6zVSdKVfh6m0"
+        />
         <title>Discord Clone</title>
       </Head>
       <main>

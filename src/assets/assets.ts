65d6a0df8eb18a94355b7d2e928a6c35aa22010d
Nikolay_Cholakov <nikolaicholakov25@gmail.com>
2023-04-07 14:24:00 +0300

    WIP Added all the available avatarrs

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -8,6 +8,21 @@ import section4 from "../../public/svgs/section4.svg";
 import defaultUser from "../../public/imgs/default_user.jpg";
 import exploreBackground from "../../public/svgs/dashboard-banner.svg";
 import stars from "../../public/svgs/stars.svg";
+import bat from "../../public/svgs/avatar-bat.svg";
+import dracula from "../../public/svgs/avatar-dracula.svg";
+import dragon from "../../public/svgs/avatar-dragon.svg";
+import eagle from "../../public/svgs/avatar-eagle.svg";
+import elf from "../../public/svgs/avatar-elf.svg";
+import fish from "../../public/svgs/avatar-fish.svg";
+import medusa from "../../public/svgs/avatar-medusa.svg";
+import overlord from "../../public/svgs/avatar-overlord.svg";
+import badger from "../../public/svgs/avatar-badger.svg";
+import doctor from "../../public/svgs/avatar-doctor.svg";
+import horse from "../../public/svgs/avatar-horse.svg";
+import octopus from "../../public/svgs/avatar-octopus.svg";
+import tears from "../../public/svgs/avatar-tears.svg";
+import tiger from "../../public/svgs/avatar-tiger.svg";
+import dino from "../../public/svgs/avatar-dino.svg";
 
 export {
   clouds,
@@ -20,4 +35,19 @@ export {
   section4,
   exploreBackground,
   stars,
+  bat,
+  dracula,
+  dragon,
+  eagle,
+  elf,
+  fish,
+  medusa,
+  overlord,
+  badger,
+  doctor,
+  horse,
+  octopus,
+  tears,
+  tiger,
+  dino,
 };
--- src/collections/Forms/EditProfileForm/EditProfileForm.tsx
+++ src/collections/Forms/EditProfileForm/EditProfileForm.tsx
@@ -1,110 +1,97 @@
+import {
+  dragon,
+  bat,
+  eagle,
+  elf,
+  fish,
+  medusa,
+  overlord,
+  dracula,
+  badger,
+  doctor,
+  horse,
+  tears,
+  octopus,
+  tiger,
+  dino,
+} from "assets";
+import { useUser } from "hooks";
+import Image from "next/image";
 import React, { RefObject, useEffect, useRef, useState } from "react";
-import { firebaseApi } from "services";
+import { IUserInfo, firebaseApi } from "services";
 
 interface EditProfileFormProps {
   ref?: RefObject<HTMLFormElement>;
 }
 
+const avatars: {
+  [key: string]: string;
+} = {
+  dragon: dragon,
+  bat: bat,
+  eagle: eagle,
+  dracula: dracula,
+  elf: elf,
+  fish: fish,
+  medusa: medusa,
+  overlord: overlord,
+  badger: badger,
+  doctor: doctor,
+  horse: horse,
+  tears: tears,
+  octopus: octopus,
+  tiger: tiger,
+  dino: dino,
+};
+
 export const EditProfileForm: React.FC<EditProfileFormProps> = ({
   ...props
 }) => {
-  const [joinRoom, setJoinRoom] = useState<string>("");
-  const [successfulRoomCreation, setSuccessfulRoomCreation] =
-    useState<boolean>(false);
-  const [error, setError] = useState<string | null>();
-  const [createRoom, setCreateRoom] = useState<string>("");
-  const [icon, setIcon] = useState<string>("SiBurton");
-  const [iconsSelectorOpened, setIconsSelectorOpened] =
-    useState<boolean>(false);
+  const [userInfo, setUserInfo] = useState<IUserInfo[]>([]);
 
-  const handleChangeIcon = (icon: string) => () => {
-    setIcon(icon);
-    setIconsSelectorOpened(false);
-  };
+  useEffect(() => {
+    firebaseApi.GET.user.info(uid, setUserInfo);
 
-  const handleCreateRoomSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setSuccessfulRoomCreation(false);
-    if (createRoom.length < 2) {
-      setError("The Room name should be at least 2 characters long");
-      return;
-    }
-    const res = await firebaseApi.POST.room.create(createRoom, icon);
-    if (!res) {
-      setCreateRoom("");
-      setSuccessfulRoomCreation(true);
-      setError(null);
-    } else {
-      setError(res.error.message);
-    }
-  };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return (
+  const { user } = useUser();
+  if (!user) return null;
+
+  console.log(userInfo);
+
+  const { uid } = user;
+
+  return userInfo[0] ? (
     <form
-      className="form pt-[60px]"
-      onSubmit={(e) => handleCreateRoomSubmit(e)}
+      className="edit-profile-form"
+      // onSubmit={(e) => handleCreateRoomSubmit(e)}
     >
-      <div className="form-demo-info">
-        <h1 className="text-[18px] mb-1 md:mb-6">Create your own room!</h1>
-      </div>
-      <div>
-        <div className="create-room-icon-container">
-          <label className="form-label" htmlFor="createRoom">
-            Room Icon
-          </label>
-          <div className="create-room-icon">
-            {/* <RoomIcon size={35} fill="white" /> */}
-            <div
-              onClick={() => setIconsSelectorOpened(!iconsSelectorOpened)}
-              className="change-room-icon"
-            >
-              Change Icon
+      <div className="edit-profile-avatar">
+        <span className="block text-center py-[10px]">Choose An Avatar</span>
+        <div className="avatars">
+          {Object.keys(avatars).map((avatar) => (
+            <div key={avatar} className="avatar">
+              <Image
+                className="rounded-[5px]"
+                src={avatars[avatar]}
+                alt={"dragon"}
+                style={{
+                  objectFit: "cover",
+                }}
+              />
             </div>
-          </div>
+          ))}
         </div>
-        <div
-          className={
-            iconsSelectorOpened
-              ? "room-icons-container"
-              : "room-icons-container h-[0px] transition-all duration-1000 my-0"
-          }
-        >
-          {/* {Object.keys(icons).map((icon) => {
-            const Icon = icons[icon];
-            return (
-              <div
-                onClick={handleChangeIcon(icon)}
-                className="room-icon-choice"
-                key={icon}
-              >
-                <Icon size={35} />
-              </div>
-            );
-          })} */}
-        </div>
-
-        <label className="form-label" htmlFor="createRoom">
-          Room Name
-        </label>
-        <input
-          type="text"
-          id="createRoom"
-          value={createRoom}
-          placeholder="Room Name..."
-          className={"input"}
-          onChange={(e) => setCreateRoom(e.target.value)}
-        />
-        {error && <span className="form-error">{error}</span>}
-        {successfulRoomCreation && (
-          <span className="form-error text-green-500">Room Created!</span>
-        )}
-        <button
-          className="w-full p-2 py-[10px] border-2 rounded-[10px] bg-black/90 text-white mt-[10px]"
-          type="submit"
-        >
-          Create
-        </button>
       </div>
+      <button
+        type="submit"
+        className="my-[15px] text-center w-full text-white block"
+      >
+        Cancel
+      </button>
     </form>
+  ) : (
+    <form className="edit-profile-form"></form>
   );
 };
